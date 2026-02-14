b1fcfd2d247d981bc405403b2e944707489df9a1
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-02-14 19:03:40 -0300

    Create camera_buffer.h

---
+++ src/game/camera_buffer.h
@@ -0,0 +1,9 @@
+#include <n64>
+
+    if X = render;
+
+#define X X; {Z, FOV.Change.Scrape.Previous.Fov, Get.Fov, Change.Fov.1, Changeback.Scrape.1 ;}
+
+#define X X; lambda(0/0; {crash; false} )
+
+;
