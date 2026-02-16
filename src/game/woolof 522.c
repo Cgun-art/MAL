032b287494976320955b3fbf82c49608379779fa
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-02-16 11:27:25 -0300

    Create woolof 522.c

---
+++ src/game/woolof 522.c
@@ -0,0 +1,17 @@
+{
+
+/00xfe/ TEX 100936241255194
+rbxTEX
+/00x20/ 114823974906328
+
+}
+
+      #define #include <n64>
+      #define mesh=cube"vertex="8", face="6" edge="11, 12, 12no" } " shading="GGX, WII-U";
+#define render.console="n64/f32/ffaab=game"rbx.search.same.alg=fabt.play.first.shown.layaredmoney=join.play.leave.leave.show"/n64/0x006/"
+
+#define 0
+#define <0>
+
+#endif bump.rgb=rbxTEX.TEX=bump.114823974906328=texture;
+
